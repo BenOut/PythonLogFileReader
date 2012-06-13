b9ccff56b1dcad6484d68ef0e0ab934df3613e17
Ben Raithby <ben.raithby@hotmail.co.uk>
2012-06-13 12:31:43 +0200

    Quick update to... - String together the folder path and file name vars into a single var - Renamed a few vars to make their meaning clearer

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import os
-file_path = "C:\BEN\\text scrape test\\000 - logs\\"
-files = os.listdir(file_path)
+folder_path = "C:\BEN\\text scrape test\\000 - logs\\"
+files = os.listdir(folder_path)
 files = filter(lambda x: x.find("log0") >-1, files)
-name_n_timestamp = dict([(x, os.stat(file_path+x).st_mtime) for x in files])
-print max(name_n_timestamp, key=lambda k: name_n_timestamp.get(k))
+name_n_timestamp = dict([(x, os.stat(folder_path+x).st_mtime) for x in files])
+newest_log_file_name = max(name_n_timestamp, key=lambda k: name_n_timestamp.get(k))
+print "The latest log file is " + newest_log_file_name
+file_path = folder_path+newest_log_file_name
+print "File path = " + file_path
+
 
 raw_input("Press enter to continue")
 
